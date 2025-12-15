3e19b97f93c778ff68badc2b819fb9510eb8a31a
Sammy Kariuki <sammykariuki099@gmail.com>
2025-12-15 10:33:01 +0300

    add driver and hibernate dependencies

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Book {
+    private String bid;
+    private String title;
+    private String genre;
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
